953f5660c6c28c591dae264b4a05f03c9a26cef9
Jonathan Hsieh <jonathanh1386@gmail.com>
2019-09-14 17:41:46 -0700

    wtf

--- reply.py
+++ reply.py
@@ -5,12 +5,20 @@ imforms = ["i am", "i'm", "i a m", "i ' m", "i m", "im"]
 async def checkIm(message):
     for form in imforms:
         nm = message.content.lower()
+        stind = -1
+
+        if(nm.startswith(form)):
+            stind = 0
+        else:
+            stind = nm.find(" " + form + " ") + 1
+
         if((form + " ") in nm):
-            sendStr = "Hi " + message.content[(nm.find(form + " ") + len(form) + 2):] + " I'm "
+            sendStr = "Hi " + message.content[(stind + len(form) + 1):] + " I'm "
             if(random.random() <= 0.05):
                 sendStr += "Walter!"
             else:
                 sendStr += "walter!"
+
             await message.channel.send(sendStr)
             break
 
